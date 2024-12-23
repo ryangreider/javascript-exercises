5a394e492844b43c085d229ca94ab1102aec3a78
ryangreider <ryandaniel136@hotmail.com>
2024-12-23 11:45:08 -0500

    2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    for(i = 0; i < num; i++){
+        console.log(string);
+    }
 };
 
 // Do not edit below this line
